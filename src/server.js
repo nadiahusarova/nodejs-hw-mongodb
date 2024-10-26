e7f268286e2cef6c712047266e95538515eb420e
nadiahusarova <ngusarova911@gmail.com>
2024-10-26 14:22:09 +0300

    upd server, install bcrypt and jwt

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import expressPinoLogger from "express-pino-logger";
 import contactsRouter from "./routes/contactsRoutes.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import notFoundHandler from "./middlewares/notFoundHandler.js";
+import authRouter from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
 const logger = pino();
 const expressLogger = expressPinoLogger({ logger });
@@ -12,14 +14,13 @@ const expressLogger = expressPinoLogger({ logger });
 function setupServer() {
   const app = express();
 
+  app.use(cookieParser());
   app.use(cors());
   app.use(expressLogger);
   app.use(express.json());
-
+  app.use("/auth", authRouter);
   app.use("/contacts", contactsRouter);
-
   app.use(notFoundHandler);
-
   app.use(errorHandler);
 
   const port = process.env.PORT || 3000;
